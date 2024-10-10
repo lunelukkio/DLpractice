8d497a960ba14a3a416f014300d4ff44db151baf
lunelukkio_notePC <lunelukkio@gmail.com>
2024-10-10 11:43:46 +0900

    DL

--- 06_class_hist.py
+++ 06_class_hist.py
@@ -5,10 +5,10 @@ Created on Wed Sep 18 15:38:37 2024
 @author: lunel
 """
 
-
+# 設計図
 class Lab:
-    def __init__(self, lab_member):
-        self.student = lab_member
+    def __init__(self, lab_member_dict):
+        self.student = lab_member_dict
         
     def degree(self, person):
         if self.student[person] >= 4:
@@ -17,16 +17,29 @@ class Lab:
             print('Master')
         else:
             print('Bachelor')
+            
+    def get_all_person(self):
+        return self.student
 
 
 if __name__ == '__main__':
     lab_member = {'Sato':6, 'Suzuki':5, 'Tanaka':3}
+    # 実例１
     his_and_cyto = Lab(lab_member)
-
     his_and_cyto.degree('Sato')
     his_and_cyto.degree('Suzuki')
     his_and_cyto.degree('Tanaka')
     
-    physi_lab_member = {'Ono':0}
+    # 実例２
+    physi_lab_member = {'Ono':0, 'Saito':3}
     physiology = Lab(physi_lab_member)
     physiology.degree('Ono')
+    
+    # python recognaize a valiable as function
+    print(his_and_cyto.get_all_person()) 
+    # python recognaize a valiable as object
+    print(physiology.get_all_person)  
+    
+    # 実例３
+    test = Lab(physiology.get_all_person)
+    print(test.student())
---
+++ 08_deep_learning.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  7 11:56:50 2024
+
+@author: lulul
+"""
+
