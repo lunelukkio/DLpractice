2b90cf654d37ded865a3d538544a1ce66a93daab
lunelukkio_notePC <lunelukkio@gmail.com>
2024-10-18 16:52:52 +0900

    forward propagation

--- 09_activation_function.py
+++ 09_activation_function.py
@@ -9,14 +9,17 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-fc = nn.Linear(4, 4)
-x = torch.Tensor([-2.0, 1.0, 4, 0])
+fc = nn.Linear(5, 5)
+x = torch.Tensor([-2.0, 1.0, 4, 0, 4])
 
 x = fc(x)
 
 rl = F.relu(x)
 soft = F.softmax(x, dim=0)
 sig = torch.sigmoid(x)
+
+print("Weights:\n", fc.weight)
+print("Biases:\n", fc.bias)
 
 print(x)
 print(rl)
