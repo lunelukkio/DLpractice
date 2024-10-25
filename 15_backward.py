1bab8680ca8f41936cfbc1a78c84fa5af4691e66
lunelukkio_notePC <lunelukkio@gmail.com>
2024-10-25 09:14:34 +0900

    .

---
+++ 15_backward.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 24 10:52:56 2024
+
+@author: lulul
+"""
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+from torch import optim
+from torchvision.models import vgg11
+
+x = torch.tensor(2.0, requires_grad = True)
+a, b = 3, 5
+y = a*x + x + b
+print(y)
+y.backward()
+for i in range(4):
+    print(x.grad)
---
+++ 16_.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 24 11:02:29 2024
+
+@author: lulul
+"""
+
